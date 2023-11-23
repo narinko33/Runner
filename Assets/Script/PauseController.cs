af59a3862c9d7dd54b053fb6c1c0111e842e94d4
sasakiy916 <sasaki3y98521j@gmail.com>
2023-11-23 20:34:56 +0900

    ボタン押下直後にジャンプする問題解消

--- Assets/Script/PauseController.cs
+++ Assets/Script/PauseController.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class PauseController : MonoBehaviour
 {
     public GameObject pauseCanvas;
+    public GameController gameController;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +18,31 @@ public class PauseController : MonoBehaviour
     //ポーズ画面
     public void Pause()
     {
+        Debug.Log("Pause実行");
         //ポーズボタン押したらpausecanvasが出てくる
         pauseCanvas.SetActive(true);
         //ポーズ
         Time.timeScale = 0;
+        // 佐々木修正案
+        gameController.SwitchPause();
 
     }
     //ポーズ画面解除
     public void Active()
     {
+        Debug.Log("Active");
         //ステージシーンへ戻る
         pauseCanvas.SetActive(false);
         //ポーズ解除
         Time.timeScale = 1;
+        // 佐々木修正案
+        StartCoroutine(ReleasePause());
+    }
 
+    IEnumerator ReleasePause()
+    {
+        yield return new WaitForSeconds(0.1f);
+        gameController.isPause = false;
     }
 
     // ポーズ画面から「タイトルに戻る」ボタン押してタイトルシーンへ戻る
